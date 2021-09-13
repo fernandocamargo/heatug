b8e0267ccf4ff23368f6e583bd23daa278a2a6d9
Fernando Camargo <camargodelbuono@gmail.com>
2021-09-13 05:45:05 +0200

    ✨

--- src/components/root/render.js
+++ src/components/root/render.js
@@ -1,4 +1,5 @@
 import React, { forwardRef } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider as Theming } from '@mui/material/styles';
 
 import theme from 'themes/default';
@@ -7,6 +8,8 @@ import { Style } from 'components';
 export default forwardRef(({ render: Render, props }, ref) => (
   <Theming theme={theme}>
     <Style />
-    <Render {...props} ref={ref} />
+    <Router>
+      <Render {...props} ref={ref} />
+    </Router>
   </Theming>
 ));
